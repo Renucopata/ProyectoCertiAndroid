0233b0b1029cc5a7b354f2eb25a2286937a7eef4
Juan Cla C <juanclaudiocarrascotapia@gmail.com>
2023-04-22 14:34:51 -0400

    commit bottomnav

--- app/src/main/java/com/first/mascotapp/MainActivity.kt
+++ app/src/main/java/com/first/mascotapp/MainActivity.kt
@@ -2,13 +2,25 @@ package com.first.mascotapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNavView.setupWithNavController(navController)
     }
     override fun onStart() {
         super.onStart()
     }
+    private lateinit var navController: NavController
+
+
 }
