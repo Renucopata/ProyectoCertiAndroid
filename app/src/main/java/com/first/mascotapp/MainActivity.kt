ba19ed5f7db9fd6e41e2b040d034ddfd66512c2a
Adrian Rodriguez <adrian.rodriguez@mojix.com>
2023-04-23 10:56:33 -0400

    Fix compilacion

--- app/src/main/java/com/first/mascotapp/MainActivity.kt
+++ app/src/main/java/com/first/mascotapp/MainActivity.kt
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.first.mascotapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        navController = findNavController(R.id.nav_host_fragment)
+        navController = binding.navHostFragment.getFragment<NavHostFragment>().navController
 
         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
         bottomNavView.setupWithNavController(navController)
