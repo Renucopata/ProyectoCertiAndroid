d388e3ffe2d79e35d1049db4610df6d1a564c045
Rene Rengel <Rene.Rengel@jala.university>
2023-04-25 02:21:05 -0400

    Add navigation to vetFragment

--- app/src/main/java/com/first/mascotapp/fragments/ServicesFragment.kt
+++ app/src/main/java/com/first/mascotapp/fragments/ServicesFragment.kt
@@ -5,29 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.first.mascotapp.R
 import com.first.mascotapp.adapters.ServicesAdapter
 import com.first.mascotapp.databinding.FragmentServicesBinding
+import com.first.mascotapp.fragments.viewModels.ServicesViewModel
 
 class ServicesFragment : Fragment() {
     lateinit var binding: FragmentServicesBinding
-    private val images = listOf(
+
+     val servicesViewModel: ServicesViewModel by activityViewModels()
+   /* private val images = listOf(
         "https://example.com/image1.jpg",
         "https://example.com/image2.jpg",
         "https://example.com/image3.jpg"
-    )
+    )*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentServicesBinding.inflate(inflater, container, false)
+        binding.servicesViewModel = servicesViewModel
+        binding.lifecycleOwner = this
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.rvServices.adapter = ServicesAdapter(images)
+        binding.btnVets.setOnClickListener {
+            view.findNavController().navigate(R.id.vetsFragment)
+        }
+       // binding.rvServices.adapter = ServicesAdapter(images)
 
     }
+
+
 }
---
+++ app/src/main/java/com/first/mascotapp/fragments/viewModels/ServicesViewModel.kt
@@ -0,0 +1,22 @@
+package com.first.mascotapp.fragments.viewModels
+
+import android.view.View
+import androidx.lifecycle.ViewModel
+import com.first.mascotapp.R
+import com.first.mascotapp.fragments.VetsFragment
+
+class ServicesViewModel: ViewModel() {
+
+
+
+
+
+    /*fun onBtnOtrosClicked(view: View) {
+        val fragment = VetsFragment()
+        val fragmentManager = requireActivity().supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.serviceFragContainer, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }*/
+}
