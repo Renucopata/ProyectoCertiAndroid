54655e68056dd2a7b9d88a460dc61debcbe2386c
Rene Rengel <Rene.Rengel@jala.university>
2023-04-28 09:03:33 -0400

    fix imports

--- app/src/main/java/com/first/mascotapp/ClienteApi.kt
+++ app/src/main/java/com/first/mascotapp/ClienteApi.kt
@@ -1,11 +1,17 @@
 package com.first.mascotapp
 
+import com.first.mascotapp.models.ServiceListItem
+import kotlinx.coroutines.flow.Flow
+import me.sianaki.flowretrofitadapter.FlowCallAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Url
+
 
 class ClienteApi {
 
-    /*val retrofit = Retrofit.Builder()
+    val retrofit = Retrofit.Builder()
         .baseUrl("http://192.168.0.6:8000/") // TODO: Aqui reemplazar por su IP de su server mock!
         .addCallAdapterFactory(FlowCallAdapterFactory.create()) // Requerido para utilizar flow para las llamadas asincronas
         .addConverterFactory(GsonConverterFactory.create()) // Requerido para la serializacion/deserializacion
@@ -14,13 +20,15 @@ class ClienteApi {
     //Como ultimo paso, definir una interfaz que exponga los metodos que tiene nuestro servidor mock, y crear una instancia de un cliente retrofit con ello, por ejemplo:
 
     interface Api {
-        @GET("stores/list")
-        fun getStoresList(): Flow<List<HomeListItem>>
+       @GET("services/list")
+        fun getStoresList(): Flow<List<ServiceListItem>>
 
-        @GET("stores/{id}")
-        fun getStoreById(@Url id: String): Flow<List<HomeListItem>>
+
+        @GET("services/{id}")
+        fun getStoreById(@Url id: String): Flow<List<ServiceListItem>>
+
     }
 
-   val client = retrofit.create(Api::class.java)*/
+   val client = retrofit.create(Api::class.java)
 
 }
