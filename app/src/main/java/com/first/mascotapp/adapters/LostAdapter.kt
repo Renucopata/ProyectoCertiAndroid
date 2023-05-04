87ebe84e9eba2ae551ca2208ada08ac95cf79337
Rene Rengel <Rene.Rengel@jala.university>
2023-05-04 09:59:17 -0400

    Recycler view with binding

--- app/src/main/java/com/first/mascotapp/adapters/LostAdapter.kt
+++ app/src/main/java/com/first/mascotapp/adapters/LostAdapter.kt
@@ -1,21 +1,31 @@
 package com.first.mascotapp.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.first.mascotapp.databinding.LostItemBinding
 import com.first.mascotapp.models.lostItemModel
 
 class LostAdapter (val itemList : List<lostItemModel>) : RecyclerView.Adapter<LostListViewHolder>() {
+
+
+    //Inflate the layout
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LostListViewHolder {
-        TODO("Not yet implemented")
+        return LostListViewHolder(LostItemBinding.inflate(LayoutInflater.from(parent.context),parent, false))
     }
+
+
+    //Here you have to set the holder that you created down, and set the view with the values that you are going to find in your list
+    //of models
 
     override fun onBindViewHolder(holder: LostListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.binding.lostListItem = itemList[position]
     }
-
+    //Here you get the size of the list
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return itemList.size
     }
 
 }
-class LostListViewHolder(val binding: lostItemModel): RecyclerView.ViewHolder(binding.root)
+//Here you must declare the views on your layout
+class LostListViewHolder(val binding: LostItemBinding): RecyclerView.ViewHolder(binding.root)
