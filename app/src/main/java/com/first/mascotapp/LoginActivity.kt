d2faf1a9ff07d45f96b0cc68586f4020d96c27dc
Rene Rengel <Rene.Rengel@jala.university>
2023-05-08 14:22:25 -0400

    Add register and login activities to the navigation flow

--- app/src/main/java/com/first/mascotapp/LoginActivity.kt
+++ app/src/main/java/com/first/mascotapp/LoginActivity.kt
@@ -1,13 +1,18 @@
 package com.first.mascotapp
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Button
+import com.first.mascotapp.databinding.ActivityLoginBinding
+import com.first.mascotapp.databinding.ActivityRegisterBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE) // will hide the title
@@ -21,6 +26,19 @@ class LoginActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         } // enable full screen
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val loginButton: Button = findViewById(R.id.login_btn)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val otherButton: Button = findViewById(R.id.registrate_btn)
+        otherButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/first/mascotapp/RegisterActivity.kt
+++ app/src/main/java/com/first/mascotapp/RegisterActivity.kt
@@ -1,13 +1,18 @@
 package com.first.mascotapp
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Button
+import com.first.mascotapp.databinding.ActivityMainBinding
+import com.first.mascotapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE) // will hide the title
@@ -21,6 +26,13 @@ class RegisterActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         } // enable full screen
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val createAccount: Button = findViewById(R.id.btnCreateAccount)
+        createAccount.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
