6e482b3f831b8ab75feb51dae645221c86aa2112
NatyBilbao <natabilbaocano18@gmail.com>
2023-05-08 11:47:21 -0400

    TrackingFragment binding

--- app/src/main/java/com/first/mascotapp/fragments/TrackingFragment.kt
+++ app/src/main/java/com/first/mascotapp/fragments/TrackingFragment.kt
@@ -6,13 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.first.mascotapp.R
+import com.first.mascotapp.databinding.FragmentMapsBinding
+import com.first.mascotapp.databinding.FragmentTrackingBinding
 
 class TrackingFragment : Fragment() {
+
+    private var _binding: FragmentTrackingBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_tracking, container, false)
+        _binding = FragmentTrackingBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
