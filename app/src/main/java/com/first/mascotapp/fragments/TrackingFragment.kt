606711d4c46cfdf845245bc7263f7c3121d99b07
NatyBilbao <natabilbaocano18@gmail.com>
2023-05-08 14:22:46 -0400

    Revert "TrackingFragment binding"
    This reverts commit 6e482b3f831b8ab75feb51dae645221c86aa2112.

--- app/src/main/java/com/first/mascotapp/fragments/TrackingFragment.kt
+++ app/src/main/java/com/first/mascotapp/fragments/TrackingFragment.kt
@@ -6,20 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.first.mascotapp.R
-import com.first.mascotapp.databinding.FragmentMapsBinding
-import com.first.mascotapp.databinding.FragmentTrackingBinding
 
 class TrackingFragment : Fragment() {
-
-    private var _binding: FragmentTrackingBinding? = null
-
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentTrackingBinding.inflate(inflater, container, false)
-        return binding.root
+        return inflater.inflate(R.layout.fragment_tracking, container, false)
     }
 }
